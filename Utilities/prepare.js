c30b0bef1600c0a01d9aeac6a7cb4fd24c6a3688
Angel Flores <angel.flores@q.bio>
2022-01-14 12:11:38 -0800

    Add custom warning message

--- Utilities/prepare.js
+++ Utilities/prepare.js
@@ -3,6 +3,7 @@ if (process.env.CI === undefined) {
     require('husky').install();
   } catch (e) {
     // error out, but don't break install
+    console.warn('Something went wrong aflores');
     console.warn('[husky warning]:', e.message);
   }
 }
